ef95b0f43a8e6d2f7f64846e6a81a74b0cbc489d
Oleksandr Kovalchuk <anxolerd@outlook.com>
2021-04-01 07:38:54 +0300

    Add starting code example for 00-no-framework

--- 00-no-framework/test.py
+++ 00-no-framework/test.py
@@ -22,8 +22,20 @@ class HeapsterWatch:
         return self._minutes
 
     def to_12_hours(self) -> str:
-        pass
-        # TODO: implement
+        am_pm = 'AM'
+        hours = self._hours
+        minutes = self._minutes
+
+        if hours >= 12:
+            am_pm = 'PM'
+
+        if hours == 0:
+            hours = 24
+
+        if hours > 12:
+            hours -= 12
+
+        return '%02d:%02d %2s' % (hours, minutes, am_pm)
 
 
 # =====
@@ -31,7 +43,17 @@ class HeapsterWatch:
 # =====
 
 def run_tests():
-    pass
+    watch = HeapsterWatch(0, 0)
+    print(watch.to_12_hours())  # should be 12:00 AM
+
+    watch = HeapsterWatch(11, 43)
+    print(watch.to_12_hours())  # should be 11:43 AM
+
+    watch = HeapsterWatch(12, 0)
+    print(watch.to_12_hours())  # should be 12:00 PM
+
+    watch = HeapsterWatch(23, 43)
+    print(watch.to_12_hours())  # should be 11:43 PM
 
 
 if __name__ == '__main__':

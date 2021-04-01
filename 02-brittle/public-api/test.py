f5fedc33bb864159d5941313717b837be3fc60df
Oleksandr Kovalchuk <anxolerd@outlook.com>
2021-04-01 08:16:20 +0300

    Fix public-api example

--- 02-brittle/public-api/test.py
+++ 02-brittle/public-api/test.py
@@ -2,9 +2,6 @@ import unittest
 
 
 class HeapsterWatch:
-    _hours: int
-    _minutes: int
-
     def __init__(self, hours: int = 0, minutes: int = 0):
         if (hours < 0 or hours > 23):
             raise ValueError(
@@ -13,31 +10,35 @@ class HeapsterWatch:
         if (minutes < 0 or minutes > 59):
             raise ValueError(
                 'Minutes should be in range [0, 59], got %s' % minutes)
-        self._hours = hours
-        self._minutes = minutes
+        # self._hours = hours
+        # self._minutes = minutes
 
-        # self._minutes = 60 * hours + minutes
+        self._minutes = 60 * hours + minutes
 
     def display(self) -> str:
-        # hours = (self.minutes // 60) % 24
-        # minutes = (self.minutes % 60)
-        hours = self._hours
-        minutes = self._minutes
+        hours = (self._minutes // 60) % 24
+        minutes = (self._minutes % 60)
+        # hours = self._hours
+        # minutes = self._minutes
         return "%02d:%02d" % (hours, minutes)
 
     def inc_hours(self):
-        # self._minutes += 60
-        self._hours += 1
+        self._minutes += 60
+        self._minutes %= 1440
+        # self._hours += 1
 
     def dec_hours(self):
-        # self._minutes -= 60
-        self._hours -= 1
+        self._minutes -= 60
+        self._minutes %= 1440
+        # self._hours -= 1
 
     def inc_minutes(self):
         self._minutes += 1
+        self._minutes %= 1440
 
     def dec_minutes(self):
         self._minutes -= 1
+        self._minutes %= 1440
 
 
 # =====
@@ -56,4 +57,4 @@ class TestHeapsterWatch(unittest.TestCase):
         watch.inc_hours()
 
         # Assert
-        self.assertEqual(watch._hours, 11)
+        self.assertEqual(watch._minutes, 673)
